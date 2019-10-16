d467225de8bb485bbaab12bd215ccb35190fab5f
handeArpakus <43812740+handeArpakus@users.noreply.github.com>
2019-10-16 23:14:08 +0300

    update

--- js/creative.js
+++ js/creative.js
@@ -42,7 +42,7 @@
   // Magnific popup calls
   $('#portfolio').magnificPopup({
     delegate: 'a',
-    type: 'image',
+    
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
     gallery: {
